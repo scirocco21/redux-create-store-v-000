3f615882a756d4217eb129bfd65ecae342057bb7
Sebastian <sebgertz@gmail.com>
2018-09-24 22:53:49 -0400

    finished readme, added comments

--- reducer.js
+++ reducer.js
@@ -1,4 +1,23 @@
-let state;
+// createStore function is completely general and can be called with a specific reducer that is elsewhere defined
+function createStore(reducer) {
+  // put state inside function instead of making it global variable
+  let state;
+
+  // move dispatch() into createStore so it can access the state variable
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+  // return objects with two functions, one to retrieve state, one to update it via reducer
+  return {
+    dispatch,
+    getState
+  };
+};
 
 function changeCount(state = { count: 0 }, action) {
   switch (action.type) {
@@ -8,21 +27,18 @@ function changeCount(state = { count: 0 }, action) {
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = changeCount(state, action);
-  render();
-};
+}
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  // render now needs store to call its own getState() method, which in turn has the property of count
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
+let store = createStore(changeCount) // createStore takes the changeCount reducer as an argument
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
